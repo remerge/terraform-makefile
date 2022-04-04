9d20854eb20557808c4dbe6111b6f1ef4daa6306
Benedikt Böhm <bb@xnull.de>
2022-04-04 16:07:08 +0200

    add pre-commit

--- build.mk
+++
@@ -1,2 +0,0 @@
-build:: ## generate a speculative execution plan, showing what actions Terraform would take to apply the current configuration
-	$(TERRAFORM) plan -compact-warnings -out=terraform.plan
--- prepare.mk
+++
@@ -1,9 +0,0 @@
-clean:: ## remove Terraform modules and initialized backend configuration
-	rm -rf .terraform
-
-install:: ## install Terraform providers and modules
-	$(TERRAFORM) init
-
-update:: ## update Terraform providers and modules
-	$(TERRAFORM) init -upgrade
-	$(TERRAFORM) providers lock -platform={darwin,linux}_{amd64,arm64}
